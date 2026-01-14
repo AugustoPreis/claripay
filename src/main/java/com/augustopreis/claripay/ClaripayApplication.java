717aea15bfe7c5bedbde66d277b1b797533c311e
Augusto Tomasi <augustopreisthomasi@gmail.com>
2026-01-14 08:16:40 -0300

    feat: adiciona classe principal da aplicação Spring Boot

---
+++ src/main/java/com/augustopreis/claripay/ClaripayApplication.java
@@ -0,0 +1,13 @@
+package com.augustopreis.claripay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaripayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaripayApplication.class, args);
+	}
+
+}
